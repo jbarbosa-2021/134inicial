33f413e19bc413c067ed21c42acfe1a9d5b65d08
jbarbosa-2021 <jbarbosa.jabb@gmail.com>
2022-12-07 21:42:11 -0300

    Criaçáo de teste de unidade com leitura de lista de valores

---
+++ tests/unit/test_main.py
@@ -0,0 +1,134 @@
+import pytest
+
+from main import somar, subtrair, multiplicar, dividir
+
+
+def teste_somar():
+    # 1 - Configura
+    numero_a = 8
+    numero_b = 7
+    resultado_esperado = 15
+
+    # 2 - Executa
+    resultado_obtido = somar(numero_a, numero_b)
+
+    # 3 - Valida
+    assert resultado_obtido == resultado_esperado
+
+
+def teste_subtrair():
+    # 1 - Prepara / Configura
+    # 1.1 - Dados de Entrada / Valores do Teste
+    numero_a = 15
+    numero_b = 10
+
+    # 1.2 - Resultados Esperados
+    resultado_esperado = 5
+
+    # 2 - Executa
+    resultado_obtido = subtrair(numero_a, numero_b)
+
+    # 3 - Valida / Checa
+    assert resultado_obtido == resultado_esperado
+
+
+def teste_multiplicar():
+    # 1 - Configure
+    numero_a = 3
+    numero_b = 4
+    resultado_esperado = 12
+
+    # 2 - Executa
+    resultado_obtido = multiplicar(numero_a, numero_b)
+
+    # 3 - Valida
+    assert resultado_obtido == resultado_esperado
+
+
+def teste_dividir_positivo():
+    # 1 - Configura
+    # 1.1 - Dados de Entrada
+    numero_a = 27
+    numero_b = 3
+
+    # 1.2 - Resultados Esperados
+    resultado_esperado = 9
+
+    # 2 - Executa
+    resultado_obtido = dividir(numero_a, numero_b)
+
+    # 3 - Valida
+    assert resultado_obtido == resultado_esperado
+
+
+def teste_dividir_negativo():
+    # 1 - Configura
+    # 1.1 - Dados de Entrada
+    numero_a = 27
+    numero_b = 0
+
+    # 1.2 - Resultados Esperados
+    resultado_esperado = 'Não dividiras por zero'
+
+    # 2 - Executa
+    resultado_obtido = dividir(numero_a, numero_b)
+
+    # 3 - Valida
+    assert resultado_obtido == resultado_esperado
+
+
+# lista para uso como massa de teste
+lista_de_valores = [
+    (8, 7, 15),
+    (20, 30, 50),
+    (25, 0, 25),
+    (-5, 12, 7),
+    (6, -3, 3)
+]
+
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', lista_de_valores)
+def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
+    # 1 - Configura
+    # utilizamos a lista como massa de teste
+
+    # 2 - Executa
+    resultado_obtido = somar(numero_a, numero_b)
+
+    # 3 - Valida
+    assert resultado_obtido == resultado_esperado
+
+# TDD = Desenvolvimento Orientado a Testes
+# Desenvolvimento Direcionado por Teste
+#
+# - Criar todos os testes de unidade no começo
+# - Executar todos os testes pelo menos 1 vez por dia
+#
+# Imagine que você no 1º dia (nada pronto)
+# Você executa todos os testes - o que acontece?
+# Dia 01 - Falhou 100 - Passou 000
+# Dia 02 - Falhou 095 - Passou 005
+# Dia 03 - Falhou 090 - Passou 010
+# Dia 04 - Falhou 088 - Passou 012
+# Dia 05 - Falhou 081 - Passou 019
+# Dia 06 - Falhou 075 - Passou 025
+# Informação sobre o progresso
+# Insistir mais um dia 1 + 1 = 2?
+# Pedir ajuda 1 + 2 = 3
+# Devolver e pegar outra 1 + 1 = 2!
+# Tudo certo! 1 + 2 = 4
+# TDD: Teste é uma medida de progresso
+
+# CI: Integração Contínua
+# IC: Integração Contínua
+
+# (Build) --> Suite de Testes ------> (Build)
+# Automatizada Passou
+# Ambiente Então, mova >> Ambiente
+# de Desenvolvimento de Teste
+
+# CD: Entrega Contínua
+# EC: Entrega Contínua
+
+# (Build) --> Suite --> (Build) -----> (Build)
+# Dev de Teste Teste Muitos Produção
+# Testes
